662eb6a5766c4f11cede03e6ffe62f2afd375ba6
Jones Thayil <38972098+jthayil@users.noreply.github.com>
2022-02-17 14:49:27 +0530

    Single File

--- main.py
+++ main.py
@@ -1,36 +1,85 @@
+import configparser
 import os
 from datetime import datetime
+
+from jinja2 import Template
 from xhtml2pdf import pisa
-from random import randint
-from jinja2 import Environment, FileSystemLoader
 
 path_cwd = os.getcwd()
-path_pdfoutput = os.path.join(path_cwd, 'pdfoutput')
+config = configparser.ConfigParser()
 
-if not os.path.exists(path_pdfoutput):
-    os.makedirs(path_pdfoutput)
+wgh_report_syntax = """
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Weighbridge</title>
+    <style>
+    .textcenter {
+        text-align: center;
+    }
+    @page {
+      size: A4 portrait;
+      margin: 2cm;
+    }
+    </style>
+</head>
+<body>
+<div class="row">
+    <div class="textcenter">{{ cpy }}</div>
+    <div class="textcenter">{{ addr }}</div>
+    <div class="col-12">
+        <hr>
+    </div>
+    <table style="width:100%">
+    <tr>
+    <td><b>Date Time</b> : {{ rd }}</td>
+    <td><b>Weight</b>: {{ wgh }}</td>
+    </tr>
+    </table>
+</div>
+</body>
+</html>
+"""
 
 
-def html_to_pdf(source_html=None, output_filename=None):
-    result_file = open(output_filename, "w+b")
-    pisa_status = pisa.CreatePDF(source_html, dest=result_file)
-    result_file.close()
-    return pisa_status.err
+def html_to_pdf(rendered_html=None, pdf_filepath=None):
+    if rendered_html is None:
+        raise ValueError('Insufficent Arguements')
+    else:
+        if pdf_filepath is None:
+            pdf_filepath = str(datetime.now().strftime("%m%S%f")) + '.pdf'
+        this_pdf = open(pdf_filepath, "w+b")
+        pisa_status = pisa.CreatePDF(rendered_html, dest=this_pdf)
+        this_pdf.close()
+        if pisa_status.err:
+            return pisa_status.err
+        return pdf_filepath
 
-def html_report(name=None):
-    file_loader = FileSystemLoader('templates')
-    env = Environment(loader=file_loader)
-    template = env.get_template('report.html')
-    return template.render(company='Aarti Pvt Ltd', date_time=datetime.now(), wgh=randint(20000, 40000))
+
+def get_wgh_report(company=None, address=None, wgh=None):
+    if None in (company, address, wgh):
+        raise ValueError('Insufficient Arguments')
+    else:
+        get_template = Template(wgh_report_syntax)
+        return get_template.render(
+            cpy=company,
+            addr=address,
+            rd=datetime.now(),
+            wgh=wgh
+        )
 
 
 def main():
-    context = {
-        'company': 'Aarti Pvt Ltd',
-        'date_time': datetime.now(),
-        'wgh': randint(20000, 40000)
-    }
-    html_to_pdf(html_report(), 'pdfoutput/report.pdf')
+    config.read('ftspl.ini')
+    try:
+        company = config['DEFAULT']['company']
+        address = config['DEFAULT']['address']
+        wgh = config['DEFAULT']['wgh']
+        return html_to_pdf(get_wgh_report(company, address, wgh), 'report.pdf')
+    except KeyError:
+        raise KeyError('Config Read Failed')
 
 
 if __name__ == "__main__":
